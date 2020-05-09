7c89742fed497faef8653ac1a8e9be3aad3f0681
hamidrezayaghobi <59170724+hamidrezayaghobi@users.noreply.github.com>
2020-05-09 19:19:46 +0430

    fill all the View method's

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,40 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
+        String name="bastani";
+        String off="";
+        double price=2321.3213;
+        int i=1;
+        long id=1232312;
+        System.out.printf("%d. %-30s  %-8.2f $  %-15s Id:%d\n",i,name,price,off,id);
+
+        name="khordaniiiiii";
+        price=32.123;
+        i=2;
+        off="off~"+12+"%";
+        id=49043;
+        System.out.printf("%d. %-30s  %-8.2f $  %-15s Id:%d\n",i,name,price,off,id);
+
+        name="kiiii";
+        price=3232.123;
+        i=2;
+        off="";
+        id=49043;
+        System.out.printf("%d. %-30s  %-8.2f $  %-15s Id:%d\n",i,name,price,off,id);
+        System.out.println();
+        String firstProductName="bastani";
+        String firstProductOffDetail="off~"+12+"%";
+        String firstExplanation="asjkdaskjdnaskdnkjasd";
+        double firstProductPrice=123.3213;
+        double firstProductAverageScore=14.6;
+        System.out.printf("%-30s %-10.2f $ %-20s Score: %-10.2f Detail:%s\n",firstProductName,firstProductPrice,firstProductOffDetail,firstProductAverageScore,firstExplanation);
+        firstProductName="khordaniiiii";
+        firstProductOffDetail="";
+        firstExplanation="asdklajsdklajsdlkjasldkjaslkdjlaksdj";
+        firstProductPrice=123123.32;
+        firstProductAverageScore=100;
+        System.out.printf("%-30s %-10.2f $ %-20s Score: %-10.2f Detail:%s\n",firstProductName,firstProductPrice,firstProductOffDetail,firstProductAverageScore,firstExplanation);
+
         DataBase.startProgram();
     }
 }
--- src/main/java/View/View.java
+++ src/main/java/View/View.java
@@ -17,77 +17,136 @@ public class View {
                                        ArrayList<Double> productPrice,
                                        ArrayList<Double> offPercentage,
                                        ArrayList<Boolean> doesItHaveOff) {
-
-
-        //
+        for (int i = 0; i <productsId.size() ; i++) {
+            String off="";
+            if (doesItHaveOff.get(i)){
+                off="%OFF%"+offPercentage.get(i)+"%";
+            }
+            System.out.printf("%d. %-30s  %-9.2f $  %-15s Id:%d\n",i,productName.get(i),productPrice.get(i),off,productsId.get(i));
+        }
     }
 
     public static void printAllOffProduct(ArrayList<Long> productsId,
                                           ArrayList<String> productName,
                                           ArrayList<Double> productPrice,
                                           ArrayList<Double> offPercentage) {
-        //
+        for (int i = 0; i <productsId.size() ; i++) {
+            String off="%OFF%"+offPercentage.get(i)+"%";
+            System.out.printf("%d. %-30s  %-9.2f $  %-15s Id:%d\n",i,productName.get(i),productPrice.get(i),off,productsId.get(i));
+        }
     }
 
-    //in ba printAllProduct fargh dare ya na?
+
+    //duplicate shode alan vali be in khater hastesh ke shayad bekhaim bad az filter shodan joor digee neshon bedim
     public static void printFilteredProduct(ArrayList<Long> productsId,
                                             ArrayList<String> productName,
                                             ArrayList<Double> productPrice,
                                             ArrayList<Double> offPercentage,
                                             ArrayList<Boolean> doesItHaveOff) {
-        //
+        System.out.println("Filtered Product");
+        for (int i = 0; i <productsId.size() ; i++) {
+            String off="";
+            if (doesItHaveOff.get(i)){
+                off="%OFF%"+offPercentage.get(i)+"%";
+            }
+            System.out.printf("%d. %-30s  %-9.2f $  %-15s Id:%d\n",i,productName.get(i),productPrice.get(i),off,productsId.get(i));
+        }
     }
 
     public static void printCurrentFilter(ArrayList<String> filters) {
-        //
+        System.out.println("Current Filter's are:");
+        for (int i = 0; i < filters.size(); i++) {
+            System.out.println(i+". "+filters.get(i));
+        }
     }
 
-    //in ba printAllProduct fargh dare ya na?
+    //duplicate shode alan vali be in khater hastesh ke shayad bekhaim bad az filter shodan joor digee neshon bedim
     public static void printSortedProduct(ArrayList<Long> productsId,
                                           ArrayList<String> productName,
                                           ArrayList<Double> productPrice,
                                           ArrayList<Double> offPercentage,
                                           ArrayList<Boolean> doesItHaveOff) {
-        //
+        System.out.println("Sorted Product");
+        for (int i = 0; i <productsId.size() ; i++) {
+            String off="";
+            if (doesItHaveOff.get(i)){
+                off="%OFF%"+offPercentage.get(i)+"%";
+            }
+            System.out.printf("%d. %-30s  %-9.2f $  %-15s Id:%d\n",i,productName.get(i),productPrice.get(i),off,productsId.get(i));
+        }
     }
 
     public static void printCurrentSort(ArrayList<String> sort){
-        //
+        System.out.println("Current Sort's are");
+        for (int i = 0; i < sort.size(); i++) {
+            System.out.println(i+". "+sort.get(i));
+        }
     }
     //in ba printAllProduct fargh dare ya na?
-    public static void printOffProduct(ArrayList<Long> productsId,
-                                       ArrayList<String> productName,
-                                       ArrayList<Double> productPrice,
-                                       ArrayList<Double> offPercentage,
-                                       ArrayList<Boolean> doesItHaveOff) {
-        //
-    }
 
     public static void printCategories(ArrayList<String> categoriesName) {
-        //
+        System.out.println("Available Categories are:");
+        for (int i = 0; i < categoriesName.size(); i++) {
+            System.out.println(i+". "+categoriesName.get(i));
+        }
     }
 
     public static void printAvailableFilters(ArrayList<String> filters) {
-        //
+        System.out.println("Available Filters are:");
+        for (int i = 0; i < filters.size(); i++) {
+            System.out.println(i+". "+filters.get(i));
+        }
     }
 
     public static void printAvailableSorting(ArrayList<String> sorting) {
-        //
+        System.out.println("Available Sort's are:");
+        for (int i = 0; i < sorting.size(); i++) {
+            System.out.println(i+". "+sorting.get(i));
+        }
     }
 
     public static void printProductSummery(Long productId,String productName,
                                            double offPercentage,String explanation,
-                                           double productPrice,String Category,
+                                           double productPrice,String category,
                                            double productAverageScore) {
-        //
+        String offDetail="";
+        if (offPercentage==0){
+            offDetail="This product is not on OFF(。﹏。*)";
+        }else{
+            offDetail="This product is on OFF("+offPercentage+"%)";
+        }
+        System.out.println(productName+" INFO:"+"\n"+
+                "Product Id: "+productId+"\n"+
+                "Price: "+productPrice+"\n"+
+                offDetail+"\n"+
+                "Score: "+productAverageScore+"\n"+
+                "Category: "+category+"\n"+
+                "More Detail:\n"+explanation);
     }
 
     public static void printAttributes(Long productId,String productName,
                                        double offPercentage,String explanation,
-                                       double productPrice,String Category,
+                                       double productPrice,String category,
                                        double productAverageScore,ArrayList<String> allSeller,
                                        int remainedNumber) {
-        //
+        String offDetail="";
+        if (offPercentage==0){
+            offDetail="This product is not on OFF(。﹏。*)";
+        }else{
+            offDetail="This product is on OFF("+offPercentage+"%)";
+        }
+        System.out.println(productName+" INFO:"+"\n"+
+                "Product Id: "+productId+"\n"+
+                "Count of Remained:"+remainedNumber+"\n"+
+                "Price: "+productPrice+"\n"+
+                offDetail+"\n"+
+                "Score: "+productAverageScore+"\n"+
+                "Category: "+category+"\n"+
+                "More Detail:\n"+explanation);
+        System.out.println("you can buy this Product from:");
+        for (int i = 0; i < allSeller.size(); i++) {
+            System.out.println(" ~"+i+". "+allSeller.get(i));
+        }
     }
 
     public static void printCompareProduct(String firstProductName, double firstOffPercentage,
@@ -97,12 +156,27 @@ public class View {
                                            String secondProductName, double secondOffPercentage,
                                            String secondExplanation, double secondProductPrice,
                                            double secondProductAverageScore) {
-        //
+        System.out.println("~~~~~~~~ Comparing "+firstProductName+"Vs. "+secondProductName+" ~~~~~~~~");
+        String firstProductOffDetail="";
+        String secondProductOffDetail="";
+        if (firstOffPercentage!=0){
+            firstProductOffDetail="~OFF("+firstOffPercentage+")";
+        }
+        if (secondOffPercentage!=0){
+            secondProductOffDetail="~OFF("+secondOffPercentage+")";
+        }
+        System.out.printf("%-30s %-12.2f $ %-20s Score: %-10.2f Detail:%s\n",firstProductName,firstProductPrice,firstProductOffDetail,firstProductAverageScore,firstExplanation);
+        System.out.printf("%-30s %-12.2f $ %-20s Score: %-10.2f Detail:%s\n",secondProductName,secondProductPrice,secondProductOffDetail,secondProductAverageScore,secondExplanation);
     }
 
     public static void printAvailableBrand(ArrayList<String> availableBrand){
-
+        System.out.println("Available Brand's are:");
+        for (int i = 0; i < availableBrand.size(); i++) {
+            System.out.println(i+". "+availableBrand.get(i));
+        }
     }
+
+
     //tavabee ke az alireza type khroji ro migiram.
 
     public static void printString(String string) {
