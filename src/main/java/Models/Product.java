75a0f9f0e20eb90e98b644b4b11f124cac7989e0
hamidrezayaghobi <59170724+hamidrezayaghobi@users.noreply.github.com>
2020-05-18 11:02:36 +0430

    OffAndProductMenuController Test Complete(only one thing remain that need alireza correct disable sort) ShowProductDetail Test Complete

--- src/main/java/Models/Product.java
+++ src/main/java/Models/Product.java
@@ -271,7 +271,13 @@ public class Product implements Serializable {
     }
 
     public int getRemainingItemsForSeller(Seller seller) {
-        return availableItems.get(allSellers.indexOf(seller));
+        for (int i = 0; i < allSellers.size(); i++) {
+            if (allSellers.get(i).getUsername()==seller.getUsername()){
+                return availableItems.get(i);
+            }
+        }
+//        return availableItems.get(allSellers.indexOf(seller));
+        return 0;
     }
 
     public ArrayList<String> getAllSellerName() {
