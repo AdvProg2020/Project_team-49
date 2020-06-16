9d35e93b57a39c3d3a96d532473e65a1060c234f
alirezahr79 <heiiiiidari2000@gmail.com>
2020-06-16 19:25:34 +0430

    rating

--- src/main/java/View/Menu/OffsAndProductsMenu/ProductsPageMenu/ProductPageController.java
+++ src/main/java/View/Menu/OffsAndProductsMenu/ProductsPageMenu/ProductPageController.java
@@ -1,6 +1,7 @@
 package View.Menu.OffsAndProductsMenu.ProductsPageMenu;
 
 import Controller.Controller;
+import Models.Category;
 import Models.Product;
 import Models.Score;
 import Models.User.Guest;
@@ -52,12 +53,87 @@ public class ProductPageController implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
         product.addScore(new Score(new Guest(), 4.6, product));
         product.addScore(new Score(new Guest(), 1.6, product));
-        product.addScore(new Score(new Guest(), 2.6, product));
+        product.addScore(new Score(new Guest(), 3.6, product));
         product.addScore(new Score(new Guest(), .6, product));
+        product.addScore(new Score(new Guest(), 2.6, product));
+        product.addScore(new Score(new Guest(), 5, product));
+        product.addScore(new Score(new Guest(), 1.6, product));
+        product.addScore(new Score(new Guest(), .6, product));
+        product.addScore(new Score(new Guest(), 0.6, product));
+        product.addScore(new Score(new Guest(), 0.6, product));
+        product.addScore(new Score(new Guest(), 2.6, product));
+        product.addScore(new Score(new Guest(), 4.6, product));
+
         product = Controller.getSelectedProduct();
-        product.setAverageScore(4.8);
-        setScoreLabels();
+
         setStars();
+        setScoreBars();
+        setScoreLabels();
+        setAddressOfProduct();
+    }
+
+    private void setAddressOfProduct() {
+        ArrayList<String> address = new ArrayList<>();
+        address.add(product.getName());
+        Category category = product.getParentCategory();
+        while(category != null){
+            address.add(category.getName());
+            category = category.getParentCategory();
+        }
+        String finalAddress = "";
+        for(int i = 0 ; i < address.size() ; i++){
+            finalAddress = finalAddress + address.get(address.size() - 1 - i);
+            if(i != address.size() - 1){
+                finalAddress = finalAddress + "\\";
+            }
+        }
+        addressOfProduct.setText(finalAddress);
+        address.clear();
+    }
+
+    private void setScoreBars() {
+        int total = product.getAllScores().size();
+        int one = 0, two = 0, three = 0, four = 0, five = 0;
+        for (Score score : product.getAllScores()) {
+            double n = score.getScore();
+            if (n >= 0 && n < 1) {
+                one++;
+            } else if (n >= 1 && n < 2) {
+                two++;
+            } else if (n >= 2 && n < 3) {
+                three++;
+            } else if (n >= 3 && n < 4) {
+                four++;
+            } else if (n >= 4 && n < 5) {
+                five++;
+            }
+        }
+        double fraction1 = 0, fraction2 = 0, fraction3 = 0, fraction4 = 0, fraction5 = 0;
+        fraction1 = (1.0 * one) / (1.0 * total);
+        fraction2 = (1.0 * two) / (1.0 * total);
+        fraction3 = (1.0 * three) / (1.0 * total);
+        fraction4 = (1.0 * four) / (1.0 * total);
+        fraction5 = (1.0 * five) / (1.0 * total);
+
+        filledBar1.toFront();
+        filledBar2.toFront();
+        filledBar3.toFront();
+        filledBar4.toFront();
+        filledBar5.toFront();
+
+        System.out.println(fraction1);
+        double width1 = emptyBar1.getWidth() * fraction1;
+        double width2 = emptyBar1.getWidth() * fraction2;
+        double width3 = emptyBar1.getWidth() * fraction3;
+        double width4 = emptyBar1.getWidth() * fraction4;
+        double width5 = emptyBar1.getWidth() * fraction5;
+        System.out.println(width1);
+        filledBar1.setWidth(width1);
+        filledBar2.setWidth(width2);
+        filledBar3.setWidth(width3);
+        filledBar4.setWidth(width4);
+        filledBar5.setWidth(width5);
+        System.out.println(filledBar1.getWidth());
     }
 
     private void setScoreLabels() {
