f9199e640bc489712c9b9fad51664b3c3618e1a7
alirezahr79 <heiiiiidari2000@gmail.com>
2020-06-26 13:43:16 +0430

    manager area

--- src/main/java/View/Menu/UserArea/ManagerAreaGraphicController.java
+++ src/main/java/View/Menu/UserArea/ManagerAreaGraphicController.java
@@ -14,6 +14,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -154,6 +155,22 @@ public class ManagerAreaGraphicController implements Initializable {
     public Label editOffIdLabel;
     public ImageView upArrowRequest;
     public ImageView downArrowRequest;
+    public Pane addManagerPane;
+    public TextField usernameReg;
+    public Label userLabel;
+    public Button registerButton;
+    public PasswordField passReg;
+    public Label passLabel;
+    public PasswordField rePassReg;
+    public Label rePassLabel;
+    public TextField firstNameReg;
+    public Label fNameLabel;
+    public TextField lastNameReg;
+    public Label lNameLabel;
+    public TextField emailReg;
+    public Label emailLabel3;
+    public TextField phoneReg;
+    public Label phoneLabel;
     private Manager manager;
     private int usersIndex = 0;
     private int categoriesIndex = 0;
@@ -282,6 +299,8 @@ public class ManagerAreaGraphicController implements Initializable {
     }
 
     private void closeALlPanes() {
+        addManagerPane.setVisible(false);
+        addManagerPane.setDisable(true);
         manageRequestsPane.setVisible(false);
         manageRequestsPane.setDisable(true);
         seeRequests.setDisable(true);
@@ -862,9 +881,6 @@ public class ManagerAreaGraphicController implements Initializable {
         }
     }
 
-    public void goToAddManagerPane(ActionEvent event) {
-    }
-
     public void acceptRequest(ActionEvent event) {
 
 
@@ -891,5 +907,163 @@ public class ManagerAreaGraphicController implements Initializable {
             requestsIndex++;
         }
         setRequestsPaneContent();
+    }
+
+    public void mouseClicked(MouseEvent mouseEvent) {
+        reset();
+
+        if (mouseEvent.getSource().equals(registerButton)) {
+            register();
+        }
+        if (mouseEvent.getSource().equals(usernameReg)) {
+            usernameReg.setStyle("-fx-border-color: #1a73e8;" + "-fx-border-radius: 8;" + "-fx-background-radius: 8");
+            userLabel.setVisible(true);
+            userLabel.setTextFill(Color.valueOf("#1a73e8"));
+        }
+        if (mouseEvent.getSource().equals(passReg)) {
+            passReg.setStyle("-fx-border-color: #1a73e8;" + "-fx-border-radius: 8;" + "-fx-background-radius: 8");
+            passLabel.setVisible(true);
+            passLabel.setTextFill(Color.valueOf("#1a73e8"));
+        }
+        if (mouseEvent.getSource().equals(rePassReg)) {
+            rePassReg.setStyle("-fx-border-color: #1a73e8;" + "-fx-border-radius: 8;" + "-fx-background-radius: 8");
+            rePassLabel.setVisible(true);
+            rePassLabel.setTextFill(Color.valueOf("#1a73e8"));
+        }
+        if (mouseEvent.getSource().equals(firstNameReg)) {
+            firstNameReg.setStyle("-fx-border-color: #1a73e8;" + "-fx-border-radius: 8;" + "-fx-background-radius: 8");
+            fNameLabel.setVisible(true);
+            fNameLabel.setTextFill(Color.valueOf("#1a73e8"));
+        }
+        if (mouseEvent.getSource().equals(lastNameReg)) {
+            lastNameReg.setStyle("-fx-border-color: #1a73e8;" + "-fx-border-radius: 8;" + "-fx-background-radius: 8");
+            lNameLabel.setVisible(true);
+            lNameLabel.setTextFill(Color.valueOf("#1a73e8"));
+        }
+        if (mouseEvent.getSource().equals(emailReg)) {
+            emailReg.setStyle("-fx-border-color: #1a73e8;" + "-fx-border-radius: 8;" + "-fx-background-radius: 8");
+            emailLabel.setVisible(true);
+            emailLabel.setTextFill(Color.valueOf("#1a73e8"));
+        }
+        if (mouseEvent.getSource().equals(phoneReg)) {
+            phoneReg.setStyle("-fx-border-color: #1a73e8;" + "-fx-border-radius: 8;" + "-fx-background-radius: 8");
+            phoneLabel.setVisible(true);
+            phoneLabel.setTextFill(Color.valueOf("#1a73e8"));
+        }
+
+    }
+
+    private void register() {
+        boolean errorFound = false;
+        if (Controller.hasUserWithUsername(usernameReg.getText())) {
+            usernameReg.setStyle("-fx-border-color: #fb3449;"+"-fx-border-radius: 8;"+"-fx-background-radius: 8");
+            userLabel.setTextFill(Color.valueOf("#fb3449"));
+            errorFound = true;
+        }
+        if (!usernameReg.getText().matches("\\w+")) {
+            usernameReg.setStyle("-fx-border-color: #fb3449;"+"-fx-border-radius: 8;"+"-fx-background-radius: 8");
+            userLabel.setTextFill(Color.valueOf("#fb3449"));
+            errorFound = true;
+        }
+        if (!passReg.getText().equals(rePassReg.getText())) {
+            passReg.setStyle("-fx-border-color: #fb3449;"+"-fx-border-radius: 8;"+"-fx-background-radius: 8");
+            passLabel.setTextFill(Color.valueOf("#fb3449"));
+            rePassReg.setStyle("-fx-border-color: #fb3449;"+"-fx-border-radius: 8;"+"-fx-background-radius: 8");
+            rePassLabel.setTextFill(Color.valueOf("#fb3449"));
+            errorFound = true;
+        }
+        if (!passReg.getText().matches("\\w+")) {
+            passReg.setStyle("-fx-border-color: #fb3449;"+"-fx-border-radius: 8;"+"-fx-background-radius: 8");
+            passLabel.setTextFill(Color.valueOf("#fb3449"));
+            errorFound = true;
+        }
+        if (!rePassReg.getText().matches("\\w+")) {
+            rePassReg.setStyle("-fx-border-color: #fb3449;"+"-fx-border-radius: 8;"+"-fx-background-radius: 8");
+            rePassLabel.setTextFill(Color.valueOf("#fb3449"));
+            errorFound = true;
+        }
+        if (!firstNameReg.getText().matches("[a-zA-Z]+")) {
+            firstNameReg.setStyle("-fx-border-color: #fb3449;"+"-fx-border-radius: 8;"+"-fx-background-radius: 8");
+            fNameLabel.setTextFill(Color.valueOf("#fb3449"));
+            errorFound = true;
+        }
+        if (!lastNameReg.getText().matches("[a-zA-Z]+")) {
+            lastNameReg.setStyle("-fx-border-color: #fb3449;"+"-fx-border-radius: 8;"+"-fx-background-radius: 8");
+            lNameLabel.setTextFill(Color.valueOf("#fb3449"));
+            errorFound = true;
+        }
+        if (!emailReg.getText().matches("(\\w+)@(\\w+)")) {
+            emailReg.setStyle("-fx-border-color: #fb3449;"+"-fx-border-radius: 8;"+"-fx-background-radius: 8");
+            emailLabel.setTextFill(Color.valueOf("#fb3449"));
+            errorFound = true;
+        }
+        if (!phoneReg.getText().matches("\\d+")) {
+            phoneReg.setStyle("-fx-border-color: #fb3449;"+"-fx-border-radius: 8;"+"-fx-background-radius: 8");
+            phoneLabel.setTextFill(Color.valueOf("#fb3449"));
+            errorFound = true;
+        }
+        if (!errorFound) {
+            Controller.createAccount(getAccountInformation(usernameReg.getText(), "manager"), "manager");
+        }
+    }
+
+    private ArrayList<String> getAccountInformation(String username, String type) {
+        ArrayList<String> info = new ArrayList<>();
+        info.add(username);
+        info.add(firstNameReg.getText());
+        info.add(lastNameReg.getText());
+        info.add(emailReg.getText());
+        info.add(phoneReg.getText());
+        info.add(passReg.getText());
+        return info;
+    }
+
+    private void reset() {
+
+        if (usernameReg.getText().equals("")) {
+            userLabel.setVisible(false);
+        }
+        if (passReg.getText().equals("")) {
+            passLabel.setVisible(false);
+        }
+        if (rePassReg.getText().equals("")) {
+            rePassLabel.setVisible(false);
+        }
+        if (firstNameReg.getText().equals("")) {
+            fNameLabel.setVisible(false);
+        }
+        if (lastNameReg.getText().equals("")) {
+            lNameLabel.setVisible(false);
+        }
+        if (emailReg.getText().equals("")) {
+            emailLabel.setVisible(false);
+        }
+        if (phoneReg.getText().equals("")) {
+            phoneLabel.setVisible(false);
+        }
+
+        userLabel.setTextFill(Color.valueOf("#a9a9a9"));
+        passLabel.setTextFill(Color.valueOf("#a9a9a9"));
+        rePassLabel.setTextFill(Color.valueOf("#a9a9a9"));
+        fNameLabel.setTextFill(Color.valueOf("#a9a9a9"));
+        lNameLabel.setTextFill(Color.valueOf("#a9a9a9"));
+        emailLabel.setTextFill(Color.valueOf("#a9a9a9"));
+        phoneLabel.setTextFill(Color.valueOf("#a9a9a9"));
+
+        usernameReg.setStyle("-fx-border-color: darkgray;"+"-fx-border-radius: 8;"+"-fx-background-radius: 8");
+        passReg.setStyle("-fx-border-color: darkgray;"+"-fx-border-radius: 8;"+"-fx-background-radius: 8");
+        rePassReg.setStyle("-fx-border-color: darkgray;"+"-fx-border-radius: 8;"+"-fx-background-radius: 8");
+        firstNameReg.setStyle("-fx-border-color: darkgray;"+"-fx-border-radius: 8;"+"-fx-background-radius: 8");
+        lastNameReg.setStyle("-fx-border-color: darkgray;"+"-fx-border-radius: 8;"+"-fx-background-radius: 8");
+        emailReg.setStyle("-fx-border-color: darkgray;"+"-fx-border-radius: 8;"+"-fx-background-radius: 8");
+        phoneReg.setStyle("-fx-border-color: darkgray;"+"-fx-border-radius: 8;"+"-fx-background-radius: 8");
+    }
+
+    public void seeAddManagerPage(MouseEvent mouseEvent) {
+        System.out.println("oh fuck");
+        closeALlPanes();
+        addManagerPane.setDisable(false);
+        addManagerPane.setVisible(true);
+        System.out.println("deep shit");
     }
 }
