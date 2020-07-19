7c2e1fdc6fc8a8bd23084ab175268332d2560388
alirezahr79 <heiiiiidari2000@gmail.com>
2020-07-19 16:42:51 +0430

    shit

--- src/main/java/View/Client.java
+++ src/main/java/View/Client.java
@@ -11,6 +11,7 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Base64;
 
 public class Client {
